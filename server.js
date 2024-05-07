38b8ce95ac6b9bdaa36d04e3ebe89151fe6d4e7b
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2024-05-07 22:21:49 +0530

    host

--- server.js
+++ server.js
@@ -114,6 +114,6 @@ app.post("/todo/add", isAuthenticated, async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
     console.log(`Server is running on : http://localhost:${process.env.PORT}`);
 });
